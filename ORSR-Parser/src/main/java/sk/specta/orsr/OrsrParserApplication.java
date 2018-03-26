b7f52d634d1fc96e762b3d7acafc2a39b1e21702
jn <jn@jn-VirtualBox>
2018-03-26 18:27:20 +0200

    initial

---
+++ ORSR-Parser/src/main/java/sk/specta/orsr/OrsrParserApplication.java
@@ -0,0 +1,12 @@
+package sk.specta.orsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrsrParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrsrParserApplication.class, args);
+	}
+}
