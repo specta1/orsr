22a4d2ded7ff15c7b7f759ddc8525f4017c56435
jn <jn@jn-VirtualBox>
2018-03-28 21:10:05 +0200

    x

---
+++ ORSR-Parser/src/main/java/sk/specta/orsr/Company.java
@@ -0,0 +1,20 @@
+package sk.specta.orsr;
+
+public class Company {
+
+	String ico;
+	String name;
+	public String getIco() {
+		return ico;
+	}
+	public void setIco(String ico) {
+		this.ico = ico;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
